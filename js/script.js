aa39bcbd4bd75fdd01eebbb4f194ca5d20a9ec68
Webertt Coutinho <webertt.coutinho@gmail.com>
2023-12-24 08:43:04 -0300

    Utilizando JavaScript, para enviar parametros a tela de orçamentos

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url .includes(href)) {
+        link.classList.add("ativo");
+    }
+    
+}
+
+links.forEach(ativarLink)
+
+//Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+    console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
